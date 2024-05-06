0ee73a093dbd30bdd66576acadaf83a728e8a31e
Vitalii Koretskyi <wondertalik@gmail.com>
2024-05-06 15:53:41 +0200

    fix: set watchAllNamespaces false

--- charts/rabbitmq/install.sh
+++ charts/rabbitmq/install.sh
@@ -4,5 +4,8 @@ set -eux
 if [[ $RABBITMQ_INSTALL == true ]]
 then
   echo "RABBITMQ_VERSION: $RABBITMQ_VERSION"
-  helm upgrade --install rabbitmq charts/rabbitmq/src/rabbitmq-cluster-operator -f charts/rabbitmq/values.yaml --namespace rabbitmq-system --create-namespace
+  helm upgrade --install rabbitmq charts/rabbitmq/src/rabbitmq-cluster-operator -f charts/rabbitmq/values.yaml \
+    --set clusterOperator.watchAllNamespaces=false \
+    --set clusterOperator.watchNamespaces[0]="rabbitmq-cluster" \
+    --namespace rabbitmq-cluster --create-namespace
 fi
