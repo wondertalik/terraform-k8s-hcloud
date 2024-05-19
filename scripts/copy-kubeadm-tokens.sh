6f920522dc09f47d59b4fa954abd88437a1b556c
Vitalii Koretskyi <wondertalik@gmail.com>
2024-05-19 18:19:07 +0200

    refactor: clean scripts

--- scripts/copy-kubeadm-tokens.sh
+++ scripts/copy-kubeadm-tokens.sh
@@ -14,9 +14,3 @@ scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
     -P "${SSH_PORT}" \
     "${SSH_USERNAME}@${SSH_HOST}:/tmp/kubeadm/kubeadm_control_plane_join" \
     "${TARGET}"
-
-# scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-#     -i "${SSH_PRIVATE_KEY}" \
-#     -P "${SSH_PORT}" \
-#     "${SSH_USERNAME}@${SSH_HOST}:/tmp/kubeadm/kubeadm_config" \
-#     "${TARGET}"
